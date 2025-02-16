14d05c4045763e25733e43cd6ad5f0ee4f59b340
mazerco9 <marc.antoinediot@yahoo.fr>
2025-02-16 11:43:17 +0100

    Initial Commit

---
+++ Pawtion/PawtionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawtionApp.swift
+//  Pawtion
+//
+//  Created by Marc-Antoine Diot on 16/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct PawtionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PawtionTests/PawtionTests.swift
@@ -0,0 +1,17 @@
+//
+//  PawtionTests.swift
+//  PawtionTests
+//
+//  Created by Marc-Antoine Diot on 16/02/2025.
+//
+
+import Testing
+@testable import Pawtion
+
+struct PawtionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
